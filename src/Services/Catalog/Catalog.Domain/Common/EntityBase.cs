b1c05caeb4e212405b780f5c8001c38fb2cb8875
Ho Thanh Hung <hunght18@fpt.com.vn>
2021-11-17 15:26:58 +0700

    seed Product Data

--- src/Services/Catalog/Catalog.Domain/Common/EntityBase.cs
+++ src/Services/Catalog/Catalog.Domain/Common/EntityBase.cs
@@ -2,7 +2,7 @@ namespace Catalog.Domain.Common
 {
     public class EntityBase
     {
-        public Guid? Id { get; protected set; }
+        public Guid? Id { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public string? LastModifiedBy { get; set; }
