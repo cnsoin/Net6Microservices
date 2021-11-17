08c8d8b47c2a0288664b1ad21007c18f35c4aa59
Ho Thanh Hung <hunght18@fpt.com.vn>
2021-11-17 13:36:34 +0700

    add docker orderDb and ordering services

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/OrderDto.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/OrderDto.cs
@@ -1,6 +1,7 @@
 namespace Ordering.Application.Features.Orders.Queries.GetOrdersList;
 public class OrderDto
 {
+    public Guid? Id { get; set; }
     public string UserName { get; set; }
     public decimal TotalPrice { get; set; }
 
