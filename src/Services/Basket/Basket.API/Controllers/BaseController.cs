1e438a4fe8a884d9149ddb1743ad460795f66ab8
Ho Thanh Hung <hunght18@fpt.com.vn>
2021-11-18 16:33:12 +0700

    Redis + Basket.API

---
+++ src/Services/Basket/Basket.API/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+namespace Basket.API.Controllers
+{
+    //[Route("api/v{version:apiVersion}/[controller]")]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    //[Authorize]
+    [Produces("application/json", "application/problem+json")]
+    public class BaseController : ControllerBase
+    {
+    }
+}
