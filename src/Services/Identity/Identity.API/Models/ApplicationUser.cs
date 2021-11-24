8eaed8269ce95a41b13d735796f23dea3382e7a3
Ho Thanh Hung <hunght18@fpt.com.vn>
2021-11-24 16:55:03 +0700

    Add Identity Service

---
+++ src/Services/Identity/Identity.API/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.API.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public Guid? TenantId { get; set; }
+        public string ExtraProperties { get; set; }
+    }
+}
