a0c4ea9c4e4190b50c60eb95642f11a92a15a044
Ho Thanh Hung <hunght18@fpt.com.vn>
2021-11-25 16:50:01 +0700

    Api Login

---
+++ src/Services/Identity/Identity.API/Models/LoginResponse.cs
@@ -0,0 +1,9 @@
+namespace Identity.API.Models
+{
+    public class LoginResponse
+    {
+        public bool Successful { get; set; }
+        public string Error { get; set; }
+        public string Token { get; set; }
+    }
+}
